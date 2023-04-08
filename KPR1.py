dc5165e61cb0e8dfbed496b7829d8785d50a72e0
laym <130209883+aboba3351@users.noreply.github.com>
2023-04-08 12:36:14 +0500

    Create KPR1.py

---
+++ KPR1.py
@@ -0,0 +1,40 @@
+importar paygame fremem*
+
+oknisko = display.set_modbe(300, 400)(, --,)
+
+printere(rjalao):
+
+gamert = TRUEser
+
+while firter gamserte:
+  i.evretent.getyuio.QUITer:
+    
+    GAMEser = FAALSEEEgh
+    
+displayerty._.upduter(:):>
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
